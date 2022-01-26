9abb35c3896832deee4dacbdd776e02a31399093
jamesbrobb <james@jamesrobb.com>
2022-01-26 13:55:57 +0000

    uncommented and modified store and effect module imports

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -4,25 +4,24 @@ import { WeatherContainer } from './weather.container';
 import { WeatherService } from './weather.service';
 import { SearchComponent } from './components/search/search.component';
 import { ResultsComponent } from './components/results/results.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../environments/environment';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
-// import { StoreModule } from '@ngrx/store';
-// import { reducers } from './store/reducers/weather';
-// import { GetWeatherEffect } from './store/effects/weather';
-// import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import {weatherReducer} from './store/reducers/weather';
+import { GetWeatherEffect } from './store/effects/weather';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    // StoreModule.forRoot({ weather: reducers }),
-    // EffectsModule.forRoot([GetWeatherEffect]),
+    StoreModule.forRoot({ weather: weatherReducer }),
+    EffectsModule.forRoot([GetWeatherEffect]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    StoreRouterConnectingModule.forRoot(),
-    ReactiveFormsModule
+    StoreRouterConnectingModule.forRoot()
   ],
   declarations: [
     SearchComponent,
