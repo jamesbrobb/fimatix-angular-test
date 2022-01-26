44b2e7d0d5243cda3c3ee2b6be52acd3baab1265
jamesbrobb <james@jamesrobb.com>
2022-01-26 13:49:37 +0000

    implemented weather actions

--- src/app/weather/store/actions/weather.ts
+++ src/app/weather/store/actions/weather.ts
@@ -1,1 +1,23 @@
-// TO BE IMPLEMENTED IF YOU DECIDE TO USE NG-RX
+import {createAction, props} from "@ngrx/store";
+import {Weather} from "../../../model/weather";
+
+
+export const WEATHER_SEARCH = '[WEATHER] search';
+export const WEATHER_SEARCH_RESULT = '[WEATHER] search result';
+export const WEATHER_SEARCH_ERROR = '[WEATHER] search error';
+
+
+export const searchRequest = createAction(
+    WEATHER_SEARCH,
+    props<{city: string}>()
+)
+
+export const searchResult = createAction(
+    WEATHER_SEARCH_RESULT,
+    props<{result: Weather}>()
+)
+
+export const searchError = createAction(
+    WEATHER_SEARCH_ERROR,
+    props<{error: string}>()
+)
