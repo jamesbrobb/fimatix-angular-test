01face3dd1e859b3ae2a80d024db42797acff08c
jamesbrobb <james@jamesrobb.com>
2022-01-26 13:51:42 +0000

    implemented weather service and increased cnt to 9 to include weather for 12AM

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
+import {Weather} from "../model/weather";
+
 
 @Injectable()
 export class WeatherService {
   url = 'https://api.openweathermap.org/data/2.5/forecast';
   params = {
     q: '',
-    cnt: '8',
+    cnt: '9',
     units: 'metric',
     APPID: '010721642521f31b0fbc8c3831d45951'
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
-  searchWeatherForCity(city) {
-    // implement the service
+  searchWeatherForCity(city: string): Observable<Weather> {
+    return this._http.get(this.url, {params: {...this.params, q:city}});
   }
 }
