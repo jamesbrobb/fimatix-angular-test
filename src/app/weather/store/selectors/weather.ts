60baa5f13bc1aa346512efe58b19a08f0c2dd534
jamesbrobb <james@jamesrobb.com>
2022-01-26 13:48:16 +0000

    implemented weather selector

--- src/app/weather/store/selectors/weather.ts
+++ src/app/weather/store/selectors/weather.ts
@@ -1,1 +1,11 @@
-// TO BE IMPLEMENTED IF NG-RX IS USED
+import {createSelector} from "@ngrx/store";
+import {AppState} from "../state/state";
+import {Weather} from "../../../model/weather";
+
+
+const weatherSelector = (state: AppState) => state.weather;
+
+export const weatherResults = createSelector(
+    weatherSelector,
+    (weather: Weather[]) => weather
+)
