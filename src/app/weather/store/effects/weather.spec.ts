49b648a467b0aa17c16ded068a2914b393fe85f2
jamesbrobb <james@jamesrobb.com>
2022-01-26 13:50:03 +0000

    implemented weather effect and tests

--- src/app/weather/store/effects/weather.spec.ts
+++ src/app/weather/store/effects/weather.spec.ts
@@ -1,1 +1,68 @@
-// TO BE IMPLEMENTED IF YOU DECIDE TO USE NG-RX
+import {ActionsSubject} from "@ngrx/store";
+import {GetWeatherEffect} from "./weather";
+import {WeatherService} from "../../weather.service";
+import mockData from '../../../../mock/weather.json';
+import {Observable, of, throwError} from "rxjs";
+import {Weather} from "../../../model/weather";
+import {
+    searchError,
+    searchRequest, searchResult,
+    WEATHER_SEARCH_ERROR,
+    WEATHER_SEARCH_RESULT
+} from "../actions/weather";
+
+
+class MockWeatherService implements Pick<WeatherService, 'searchWeatherForCity'> {
+
+    constructor(private _shouldError: boolean = false) {}
+
+    searchWeatherForCity(city: string): Observable<Weather> {
+        if(this._shouldError) {
+            return throwError({error:{message: '404'}})
+        }
+        return of(mockData.weather[0])
+    }
+}
+
+type searchResultType = ReturnType<typeof searchResult>
+type searchErrorType = ReturnType<typeof searchError>
+
+
+describe('GetWeatherEffect', () => {
+
+    it('should dispatch an action on success', () => {
+
+        const mock = new MockWeatherService() as any,
+            actions = new ActionsSubject(),
+            effect = new GetWeatherEffect(actions, mock);
+
+        let result: searchResultType;
+
+        effect.getWeather$.subscribe((action: searchResultType) => {
+            result = action;
+        });
+
+        actions.next(searchRequest({city: 'London'}));
+
+        expect(result.type).toEqual(WEATHER_SEARCH_RESULT);
+        expect(result.result).toEqual(mockData.weather[0]);
+    });
+
+    it('should dispatch an action on failure', () => {
+
+        const mock = new MockWeatherService(true) as any,
+            actions = new ActionsSubject(),
+            effect = new GetWeatherEffect(actions, mock);
+
+        let result: searchErrorType;
+
+        effect.getWeather$.subscribe((action: searchErrorType) => {
+            result = action;
+        });
+
+        actions.next(searchRequest({city: 'sldkfjsj'}));
+
+        expect(result.type).toEqual(WEATHER_SEARCH_ERROR);
+        expect(result.error).toEqual('404');
+    });
+});
--- src/app/weather/store/effects/weather.ts
+++ src/app/weather/store/effects/weather.ts
@@ -1,1 +1,36 @@
-// TO BE IMPLEMENTED IF NG-RX IS USED
+import {Injectable} from "@angular/core";
+import {catchError, map, mergeMap} from "rxjs/operators";
+import {Actions, createEffect, ofType} from "@ngrx/effects";
+import {WeatherService} from "../../weather.service";
+import {
+    searchRequest,
+    WEATHER_SEARCH_ERROR,
+    WEATHER_SEARCH_RESULT
+} from "../actions/weather";
+import {Weather} from "../../../model/weather";
+import {of} from "rxjs";
+
+
+
+@Injectable()
+export class GetWeatherEffect {
+    constructor(
+        private _actions$: Actions,
+        private _weatherService: WeatherService
+    ) {}
+
+    getWeather$ = createEffect(() =>
+        this._actions$.pipe(
+            ofType(searchRequest),
+            mergeMap(({city}) => {
+                    return this._weatherService.searchWeatherForCity(city)
+                        .pipe(
+                            map((data: Weather) => ({type: WEATHER_SEARCH_RESULT, result: data})),
+                            catchError(({error}) => of({type: WEATHER_SEARCH_ERROR, error: error.message})
+                            )
+                        )
+                }
+            )
+        )
+    );
+}
